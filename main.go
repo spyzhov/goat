0bd864671dc6fbe98118dca57e05b835ee53e4f1
Pyzhov Stepan <32341341+spyzhov@users.noreply.github.com>
2018-08-09 15:54:17 +0300

    Update main.go

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ func main() {
 	app.HelpName = "goat"
 	app.Usage = "golang: application template"
 	app.Description = "Goat is golang application template generator"
-	app.Version = "0.0.3"
+	app.Version = "0.0.1"
 	app.Authors = []cli.Author{
 		{Name: "S.Pyzhov", Email: "turin.tomsk@gmail.com"},
 	}
