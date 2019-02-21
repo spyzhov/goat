c028a670d4bbc826fa375e21d0fb451b52f978d8
Stepan Pyzhov <spyzhov@ourgapps.com>
2019-02-21 13:00:28 +0300

    fix misprint

--- templates/description.go
+++ templates/description.go
@@ -181,7 +181,7 @@ func WaitExit() chan os.Signal {
 				"Dockerfile": `FROM alpine:latest as alpine
 RUN apk --no-cache add tzdata zip ca-certificates
 WORKDIR /usr/share/zoneinfo
-# -0 means no compression.  Needed because go's
+# -0 means no compression. Needed because go's
 # tz loader doesn't handle compressed data.
 RUN zip -r -0 /zoneinfo.zip .
 
