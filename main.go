3c240271eaccb294a35735d14a55750bba46df35
Stepan Pyzhov <spyzhov@ourgapps.com>
2019-02-22 17:17:01 +0300

    fix version

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ func main() {
 	app.HelpName = "goat"
 	app.Usage = "golang: application template"
 	app.Description = "Goat is golang application template generator"
-	app.Version = "0.1.3"
+	app.Version = "0.1.4"
 	app.Authors = []cli.Author{
 		{Name: "S.Pyzhov", Email: "turin.tomsk@gmail.com"},
 	}
